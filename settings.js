5952f2c0ad18e7fccbfc85a52e9e93361af5bcce
TEAM LION'S OFC <udarataroleplay@gmail.com>
2026-02-27 22:02:47 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Senu Bot',
+  author: '‎',
+  botName: "Senu Bot",
+  botOwner: 'Creator', // Your name
+  ownerNumber: '94743706283', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.7.0",
+  updateZipUrl: "https://github.com/udarataroleplay/XSENU-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
